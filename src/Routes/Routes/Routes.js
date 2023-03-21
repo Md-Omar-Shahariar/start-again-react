2345dff055659163b579b5756dff063f643325b4
Md-Omar-Shahariar <omar.shahariar@reddotdigitalit.com>
2023-03-21 16:31:25 +0600

    banner start

--- src/Routes/Routes/Routes.js
+++ src/Routes/Routes/Routes.js
@@ -1,6 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../../Layout/Main";
-import Home from "../../pages/Home/Home";
+import Home from "../../pages/Home/Home/Home";
 import Login from "../../pages/Login/Login";
 
 export const router = createBrowserRouter([
