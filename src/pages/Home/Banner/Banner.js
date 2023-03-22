4626010e56f5d37a719c81b94d6075d6244131c3
Md-Omar-Shahariar <omar.shahariar@reddotdigitalit.com>
2023-03-22 12:59:37 +0600

    background

--- src/pages/Home/Banner/Banner.js
+++ src/pages/Home/Banner/Banner.js
@@ -1,14 +1,13 @@
 import React from "react";
-import chair from "../../../assets/images/chair.png"
+import chair from "../../../assets/images/chair.png";
+import "./Banner.css";
 
 const Banner = () => {
   return (
-    <div className="hero  bg-base-100">
-      <div className="hero-content flex-col lg:flex-row-reverse">
-        <img alt=""
-          src={chair}
-          className="w-1/2 rounded-lg shadow-2xl"
-        />
+    <div className="hero ">
+      
+      <div className="hero-content   flex-col lg:flex-row-reverse">
+        <img alt="" src={chair} className="w-1/2 rounded-lg shadow-2xl" />
         <div>
           <h1 className="text-5xl font-bold">Box Office News!</h1>
           <p className="py-6">
@@ -16,7 +15,9 @@ const Banner = () => {
             excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
             a id nisi.
           </p>
-          <button className="bg-gradient-to-r from-primary to-secondary btn text-white">Get Started</button>
+          <button className="bg-gradient-to-r from-primary to-secondary btn text-white">
+            Get Started
+          </button>
         </div>
       </div>
     </div>
--- src/pages/Home/Home/Home.js
+++ src/pages/Home/Home/Home.js
@@ -1,13 +1,20 @@
-import React from 'react';
-import Banner from '../Banner/Banner';
+import React from "react";
+import Banner from "../Banner/Banner";
+import chair from "../../../assets/images/chair.png";
 
 const Home = () => {
-    return (
-        <div className='mx-5'>
-            <Banner></Banner>
-            
-        </div>
-    );
+  return (
+    <div className=" max-h-[700px] w-full ">
+      <div className="relative h-full">
+        <img
+          src={chair}
+          alt=""
+          className="absolute  opacity-25 w-full h-full  "
+        ></img>
+        <Banner></Banner>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
